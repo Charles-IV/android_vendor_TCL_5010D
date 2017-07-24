128c0b356c216fe54e245816ff070eb3813d9dd1
Charles-IV <Charles-IV@users.noreply.github.com>
2017-07-24 10:55:27 +0100

    5010D-vendor.mk: 5010D -> 4034X

--- 4034X-vendor.mk
+++ 4034X-vendor.mk
@@ -11,6 +11,6 @@ PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/TCL/5010D/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/tcl/4034X/overlay
 
-$(call inherit-product, vendor/TCL/5010D/5010D-vendor-blobs.mk)
+$(call inherit-product, vendor/tcl/4034X/4034X-vendor-blobs.mk)
