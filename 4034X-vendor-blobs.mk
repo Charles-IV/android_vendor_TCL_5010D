4918f4b4fa81840c05a4f59191e9866f7c665e18
Charles-IV <Charles-IV@users.noreply.github.com>
2017-07-24 10:54:14 +0100

    5010D-vendor-blobs.mk: 5010D -> 4034X

---
+++ 4034X-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/vendor,system/vendor)
